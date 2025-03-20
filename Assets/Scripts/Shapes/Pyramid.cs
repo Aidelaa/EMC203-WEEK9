3a900b18f5edb83542e6b5121104cf2e5d50e616
Allysa <134472134+Aidelaa@users.noreply.github.com>
2025-03-20 22:02:54 +0800

    Pyramid upd

--- Assets/Scripts/Shapes/Pyramid.cs
+++ Assets/Scripts/Shapes/Pyramid.cs
@@ -7,84 +7,71 @@ public class Pyramid : Shape, IShape
 
     public void Render(bool gizMode)
     {
-        if (!material) return;
+        if (!this.material) return;
         
         GL.PushMatrix();
         GL.Begin(GL.LINES);
-        material.SetPass(0);
+        this.material.SetPass(0);
 
         GL.Color(Color.green);
-        if (camera != null)
-            GL.LoadProjectionMatrix(camera.projectionMatrix);
-
-        Vector3[] vertices = GenerateVertices();
-        translatedVertices = new Vector3[vertices.Length];
         
+        if (this.camera != null)
+            GL.LoadProjectionMatrix(this.camera.projectionMatrix);
+
+        Vector3[] vertices = {
+            new Vector3(0f, this.size, 0f), // Top vertex
+            new Vector3(-1f, -1f, 1f) * this.size, // Back Left
+            new Vector3(1f, -1f, 1f) * this.size, // Back Right
+            new Vector3(1f, -1f, -1f) * this.size, // Front Right
+            new Vector3(-1f, -1f, -1f) * this.size // Front Left
+        };
+
+        this.translatedVertices = new Vector3[vertices.Length];
         for (int i = 0; i < vertices.Length; i++)
         {
-            translatedVertices[i] = VectorCalculations.Project(
-                VectorCalculations.Translate(vertices[i], center, rotation),
-                innerFocalLength, center.z, size
-            );
+            vertices[i] = RotateVertex(vertices[i]); // Apply rotation
+            this.translatedVertices[i] = VectorCalculations.Project(VectorCalculations.Translate(vertices[i], this.center, this.rotation), this.innerFocalLength, this.center.z, this.size);
         }
-        
-        DrawEdges(gizMode);
-        LabelVertices(gizMode);
 
+        DrawBaseEdges(gizMode);
+        ConnectBaseToTop(gizMode);
+        
         GL.End();
         GL.PopMatrix();
     }
 
-    private Vector3[] GenerateVertices()
+    private Vector3 RotateVertex(Vector3 vertex)
     {
-        return new Vector3[]
-        {
-            new Vector3(0f, size, 0f), // Top vertex
-            new Vector3(-1f, -1f, 1f) * size, // Back Left
-            new Vector3(1f, -1f, 1f) * size,  // Back Right
-            new Vector3(1f, -1f, -1f) * size, // Front Right
-            new Vector3(-1f, -1f, -1f) * size // Front Left
-        };
+        return this.center + Quaternion.Euler(this.rotation) * (vertex - this.center);
     }
-
-    private void DrawEdges(bool gizMode)
+    
+    private void DrawBaseEdges(bool gizMode)
     {
-        // Connect top vertex to base vertices
         for (int i = 1; i <= 4; i++)
         {
-            DrawLine(translatedVertices[0], translatedVertices[i], gizMode);
+            DrawLine(this.translatedVertices[i], this.translatedVertices[i % 4 + 1], gizMode);
         }
-        
-        // Connect base edges
-        DrawLine(translatedVertices[1], translatedVertices[2], gizMode);
-        DrawLine(translatedVertices[2], translatedVertices[3], gizMode);
-        DrawLine(translatedVertices[3], translatedVertices[4], gizMode);
-        DrawLine(translatedVertices[4], translatedVertices[1], gizMode);
     }
 
-    private void LabelVertices(bool gizMode)
+    private void ConnectBaseToTop(bool gizMode)
     {
-        if (!gizMode) return;
-        
-        Handles.Label(translatedVertices[0], "Top");    
-        Handles.Label(translatedVertices[1], "Back Left"); 
-        Handles.Label(translatedVertices[2], "Back Right"); 
-        Handles.Label(translatedVertices[3], "Front Right");
-        Handles.Label(translatedVertices[4], "Front Left");
+        for (int i = 1; i <= 4; i++)
+        {
+            DrawLine(this.translatedVertices[0], this.translatedVertices[i], gizMode);
+        }
     }
-
+    
     public void DrawLine(Vector3 v1, Vector3 v2, bool gizMode)
     {
         if (gizMode)
             Gizmos.DrawLine(v1, v2);
-
+        
         GL.Vertex3(v1.x, v1.y, v1.z);
         GL.Vertex3(v2.x, v2.y, v2.z);
     }
     
-    public override void OnDrawGizmos() 
-    {
-        if (!enabled) return;
+    public override void OnDrawGizmos() {
+        if (!this.enabled) return;
         Render(true);
     }
 
