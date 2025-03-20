7886203808535ce9fa664b81419403755b3eed0e
Allysa <134472134+Aidelaa@users.noreply.github.com>
2025-03-20 21:57:26 +0800

    Update full rotate

--- Assets/Scripts/Core/VectorCalculations.cs
+++ Assets/Scripts/Core/VectorCalculations.cs
@@ -1,5 +1,3 @@
-using System.Drawing;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class VectorCalculations
@@ -8,27 +6,43 @@ public class VectorCalculations
     {
         float firstAxis = axis1 * Mathf.Cos(angle) - axis2 * Mathf.Sin(angle);
         float secondAxis = axis2 * Mathf.Cos(angle) + axis1 * Mathf.Sin(angle);
-        
+
         return new Vector2(firstAxis, secondAxis);
     }
-    
+
     public static Vector3 Translate(Vector3 point, Vector3 center, Vector3 rotation)
     {
-        float radius = rotation.z * Mathf.Deg2Rad;
-        float cos = Mathf.Cos(radius);
-        float sin = Mathf.Sin(radius);
-        
-        float newX = cos * point.x - sin * point.y;
-        float newY = sin * point.x + cos * point.y;
-        
-        return new Vector3(newX + center.x, newY + center.y, point.z + center.z);
+        // Convert degrees to radians
+        float xRad = rotation.x * Mathf.Deg2Rad;
+        float yRad = rotation.y * Mathf.Deg2Rad;
+        float zRad = rotation.z * Mathf.Deg2Rad;
+
+        // Rotate around X-axis
+        float cosX = Mathf.Cos(xRad);
+        float sinX = Mathf.Sin(xRad);
+        float newY = cosX * point.y - sinX * point.z;
+        float newZ = sinX * point.y + cosX * point.z;
+
+        // Rotate around Y-axis
+        float cosY = Mathf.Cos(yRad);
+        float sinY = Mathf.Sin(yRad);
+        float newX = cosY * point.x + sinY * newZ;
+        newZ = cosY * newZ - sinY * point.x;
+
+        // Rotate around Z-axis
+        float cosZ = Mathf.Cos(zRad);
+        float sinZ = Mathf.Sin(zRad);
+        float finalX = cosZ * newX - sinZ * newY;
+        float finalY = sinZ * newX + cosZ * newY;
+
+        return new Vector3(finalX + center.x, finalY + center.y, newZ + center.z);
     }
 
     public static Vector3 Project(Vector3 point, float focalLength, float centerZ, float size)
     {
         float z = point.z + focalLength;
-        float scale = focalLength / ((centerZ - size * .5f) + focalLength);
-        
+        float scale = focalLength / ((centerZ - size * 0.5f) + focalLength);
+
         return new Vector3(point.x / scale, point.y / scale, z);
     }
 }
